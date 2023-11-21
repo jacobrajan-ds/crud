bbc09d39432b8ca8631c27a88097c50e5120596c
jacob rajan <jacobrajan.s@datasirpi.com>
2023-11-21 10:24:56 +0530

    Complete dynamic route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const user = [
+const users = [
   {
     id: 1,
     name: "Jacob",
@@ -27,6 +27,19 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   res.json(user);
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const getUser = users.filter((user) => {
+    return user.id === parseInt(id);
+  });
+
+  if (getUser.length === 0) {
+    return res.json({ error: "No user found" });
+  }
+  res.send(getUser);
 });
 
 app.post("/users", (req, res) => {});
